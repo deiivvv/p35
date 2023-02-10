0fea2919edc330eba5519c82c555b73727182ea6
deiivvv <deiivvv>
2023-02-10 10:06:35 +0100

    commit3

---
+++ src/es/Goya/Clase22.java
@@ -0,0 +1,7 @@
+package es.Goya;
+
+public class Clase22 {
+	public String metodoA() {
+		return "metodoA";
+	}
+}
---
+++ src/es/Goya/Clase31.java
@@ -0,0 +1,11 @@
+package es.Goya;
+
+public class Clase31 {
+	public String metodo1() {
+		return "metodo1";
+	}
+	
+	public String metodo2() {
+		return "metodo2";
+	}
+}
